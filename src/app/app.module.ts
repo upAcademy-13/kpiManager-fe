1f8f1eca94d7fd3110623774d3e3582439a66167
André Loureiro <andreloureiro89@gmail.com>
2020-03-23 17:57:25 +0000

    added NavBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutNavBarComponent } from './layout-nav-bar/layout-nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutNavBarComponent
   ],
   imports: [
     BrowserModule,
