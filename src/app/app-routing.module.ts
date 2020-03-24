6e02b72d3100a88f60ec0fa4bd8d36f8f63d6daa
Filipe Bastias <filipebastias94@gmail.com>
2020-03-24 15:44:34 +0000

    Path for login and layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { LayoutNavBarComponent } from './layout-nav-bar/layout-nav-bar.component';
 
 
 const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'layout', component: LayoutNavBarComponent},
   {path: '**', redirectTo: 'login'},
-  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
