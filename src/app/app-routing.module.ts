c938dd2066789e849c260826e46eb4f5de32a04b
Daniel Tavares <danieljosetavares@gmail.com>
2020-03-24 12:17:04 +0000

    Revert "Revert "Create User Template design""
    This reverts commit a58e32f548a704f1f062499aff3c232ea15448eb.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateUserComponent } from './createUser/createUser.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "createUser",
+    component: CreateUserComponent
+  },
+  {
+    path: "**",
+    redirectTo: "createUser"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
