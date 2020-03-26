9d036e7ddd860e032ea7f0ad5492e8a884857bef
Nbeites <nm.beites@gmail.com>
2020-03-26 12:42:42 +0000

    authorization by role

--- src/app/layout/layout-form/layout-form.component.ts
+++ src/app/layout/layout-form/layout-form.component.ts
@@ -15,16 +15,21 @@ export class LayoutFormComponent implements OnInit {
       selectType: number; //id do tipo de interacao
       interactionTemp:object;
       interactionTypeRow=[];
+      isAuthorized: Boolean;
 
 
   constructor() { 
     this.maxDate.setDate(this.maxDate.getDate() + 7);
     this.bsInlineRangeValue = [this.bsInlineValue, this.maxDate];
+    this.isAuthorized = this.accessVerification();
+
   }
   
 
   ngOnInit(): void {
   }
+
+
   addInteraction(){
     this.interactionTemp = {
       "name": this.selectClient, //vai ser o nome do cliente
@@ -35,6 +40,34 @@ export class LayoutFormComponent implements OnInit {
   }
   delRow(i:number){
     this.interactionTypeRow.splice(i, 1);
+  }
+
+
+  public accessVerification() {
+
+    let authorized = false;
+    //let userRole = "director"; // dummy director, this will be replaced with a query below;
+
+   // let userRole = this.http.get(this.apiUrl + '?filter={"where":{"username":"ze carlos"}}');
+
+    // If it is a document request -> this.http.get('url');
+    // url -> endpoint where the currentUser is stored
+    // Access role atribute in currentUser object -> .role ?
+
+    console.log('userRole');
+
+    // yes but it is going to have 
+
+   // if (userRole) { //   if (userRole == "director") By now it's only 2 clearance levels, Director and other (manager)
+      authorized = true;
+      console.log(authorized);
+   // }
+
+    console.log('tou no access Verification');
+ //   console.log(authorized);
+
+    return authorized;
+
   }
 
 
