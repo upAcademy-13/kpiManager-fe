32ba286acb394de8b9c8269055ca7b03c78541d4
Filipe Marques <filipeavmarques@gmail.com>
2020-03-31 20:50:49 +0100

    Criadas as funções que acedem as querys da base de dados

--- src/app/core/services/dashboard.service.ts
+++ src/app/core/services/dashboard.service.ts
@@ -1,14 +1,103 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class DashboardService {
+  constructor(private http: HttpClient) {}
 
-  constructor( private http: HttpClient) { }
+  ////////////////////
+  // Funções Micael //
+  ////////////////////
+  public getAllUnitNames() {
+    return this.http.get(
+      "http://127.0.0.1:8080/kpiManager/api/interactions/allUnits"
+    );
+  }
 
-  getAllUnits(){
-   return this.http.get("")
+  public countAllInteractionsPerUnit(unit: String) {
+    return this.http.get(
+      "http://127.0.0.1:8080/kpiManager/api/interactions/count/interactions?unit=" +
+        unit
+    );
+  }
+
+  //Apenas usar para preencher filtros da tabela, não necessário para gráficos
+  public getAllInteractionsPerUnit(unit: String) {
+    return this.http.get(
+      "http://127.0.0.1:8080/kpiManager/api/interactions/allUnitsFilter/" + unit
+    );
+  }
+
+  ////////////////////
+  // Funções Filipe //
+  ////////////////////
+  public getAllInteractionTypes() {
+    return this.http.get(
+      "http://127.0.0.1:8080/kpiManager/api/interactions/allInteractions"
+    );
+  }
+
+  public countAllInteractionsPerInteractionType(interactionType: String) {
+    return this.http.get(
+      "http://127.0.0.1:8080/kpiManager/api/interactions/count/interactions?interactionType" +
+        interactionType
+    );
+  }
+
+  //Apenas usar para preencher filtros da tabela, não necessário para gráficos
+  public getAllInteractionsPerInteractionType(interactionType: String) {
+    return this.http.get(
+      "http://127.0.0.1:8080/kpiManager/api/interactions/allInteractionsFilter/" +
+        interactionType
+    );
+  }
+
+  /////////////////
+  // Funções Ana //
+  /////////////////
+  public countAllCvsPerWeekPerManager(managerName: String, week: String) {
+    return this.http.get(
+      "http://127.0.0.1:8080/kpiManager/api/interactions/cvs/count/" +
+        managerName +
+        "?week=" +
+        week
+    );
+  }
+
+  //Apenas usar para preencher filtros da tabela, não necessário para gráficos
+  public getAllCvsPerManagerPerWeek(managerName: String, week: String) {
+    return this.http.get(
+      "http://127.0.0.1:8080/kpiManager/api/interactions/cvs/" +
+        managerName +
+        "?week=" +
+        week
+    );
+  }
+
+  ///////////////////
+  // Funções Vasco //
+  ///////////////////
+
+  public getAllClientNames() {
+    return this.http.get(
+      "http://127.0.0.1:8080/kpiManager/api/interactions/allClients"
+    );
+  }
+
+  public countAllInteractionsPerClient(clientName: String) {
+    return this.http.get(
+      "http://127.0.0.1:8080/kpiManager/api/interactions/count/interactions?clientName" +
+        clientName
+    );
+  }
+
+  //Apenas usar para preencher filtros da tabela, não necessário para gráficos
+  public getAllInteractionsPerClient(clientName: String) {
+    return this.http.get(
+      "http://127.0.0.1:8080/kpiManager/api/interactions/allClientsFilter/" +
+        clientName
+    );
   }
 }
