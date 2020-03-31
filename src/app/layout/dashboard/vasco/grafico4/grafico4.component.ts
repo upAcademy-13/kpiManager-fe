c805bff056c04c8bffa3d7698178f7c495af41d9
Filipe Marques <filipeavmarques@gmail.com>
2020-03-31 15:34:42 +0100

    Display dos gráficos na dashboard

--- src/app/layout/dashboard/vasco/grafico4/grafico4.component.ts
+++ src/app/layout/dashboard/vasco/grafico4/grafico4.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  selector: 'grafico4',
   templateUrl: './grafico4.component.html',
   styleUrls: ['./grafico4.component.scss']
 })
