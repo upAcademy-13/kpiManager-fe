c4d99f992f0427020d4b22508868470ff5e2f76e
Micael Cruz <micaelnscruz@gmail.com>
2020-04-03 16:37:08 +0200

    Graphic1 end version.

--- src/app/core/services/dashboard.service.ts
+++ src/app/core/services/dashboard.service.ts
@@ -1,7 +1,8 @@
-import { Observable } from 'rxjs';
+import { Observable, forkJoin } from 'rxjs';
 import { Unidade } from './../../layout/dashboard/micael/grafico1';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { switchMap, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,24 @@ export class DashboardService {
 
   constructor( private http: HttpClient) { }
 
-private caminho = 'assets/data/dados.json ';
 
-getAllUnits(): Observable <Unidade[]> {
-   return this.http.get<Unidade[]>(this.caminho);
-  }
-}
+public getAllUnits() {
+  return this.http
+    .get('http://127.0.0.1:8080/kpiManager/api/interactions/allUnits').pipe(
+      switchMap((units: any[]) => forkJoin(
+      units.map(unit => this.countAllInteractionsPerUnit(unit)
+        .pipe(
+          map(interaction => {
+            return {unit, interaction };
+          })
+          ))
+        ))
+      );
+    }
+
+public countAllInteractionsPerUnit(unit: String) {
+  return this.http.get(
+    "http://127.0.0.1:8080/kpiManager/api/interactions/count/interactions?unit=" +
+      unit
+  );
+}}
--- src/app/layout/dashboard/micael/grafico1/grafico1.component.ts
+++ src/app/layout/dashboard/micael/grafico1/grafico1.component.ts
@@ -26,39 +26,38 @@ escondido = true;
   ngOnInit(): void {
 
    this.ds.getAllUnits().subscribe(dados => {
-     this.listaDetalhada = dados; console.log(this.listaDetalhada); 
+     this.listaDetalhada = dados; console.log(this.listaDetalhada);
      for (const row of this.listaDetalhada) {
       this.colunas.push(row.nome);
       this.valores.push(row.valor);
-    } 
+    }
      this.myChart = new Chart(this.element.nativeElement, {
       type: 'bar',
       data: {
-        labels: this.colunas, // ['Unidade_A', 'Unidade_B', 'Unidade_C', 'Unidade_D', 'Unidade_E', 'Unidade_F', 'Nearshore', 'Porto' ],
+        labels: this.colunas,
         datasets: [{
-          label: 'Distribuição Salarial da Aubay',
-          data: this.valores,        // [3000, 2000, 4000, 3050, 2500, 1500],
+          label: 'Number of Interactions Per Business Unit ',
+          data: this.valores,
           backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
+              'rgba(251, 99, 132, 0.2)',
               'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
+              'rgba(25, 206, 86, 0.2)',
               'rgba(75, 192, 192, 0.2)',
               'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
+              'rgba(250, 159, 64, 0.2)'
           ],
           borderColor: [
-              'rgba(255, 99, 132, 1)',
+              'rgba(251, 99, 132, 1)',
               'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
+              'rgba(25, 206, 86, 1)',
               'rgba(75, 192, 192, 1)',
               'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
+              'rgba(250, 159, 64, 1)'
           ],
           borderWidth: 1
       }]
 
       },
-
 
       options: {
         responsive: false,
@@ -77,79 +76,14 @@ escondido = true;
   
   } );
 
-
-   
-
    console.log(this.listaDetalhada);
 
   }
 
   graphClickEvent(event) {
-  const idClicado: number = this.myChart.chart.getElementAtEvent(event)[0]._index;
-  this.desenhaDetalhe(idClicado);
-  this.router.navigate(['action-selection'], { state: { labelClicado: idClicado } });  console.log(idClicado);
-}
-
-// this.router.getCurrentNavigation().extras.state.example ( para eles aplicarem )
-
-desenhaDetalhe(clicado: number) {
-
-  const barraClicada: number = clicado;
-  const labelNovo: string [ ] = ['Unidade_AAA', 'Unidade_B', 'Unidade_C', 'Unidade_D', 'Unidade_E', 'Unidade_F', 'Nearshore', 'Porto' ];
-  const dadoNovo: any [ ] = [3000, 2000, 1400, 3050, 2500, 1500, 21545, 8656];
-
-  this.myDetail = new Chart(this.elemento.nativeElement, {
-    type: 'bar',
-    data: {
-      labels: labelNovo ,
-      datasets: [{
-        label: 'Distribuição Salarial da Mota Engil São Tomé',
-        data: dadoNovo,
-        backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 4
-    }]
-
-    },
-
-  });
-  this.escondido = !this.escondido;
-
-  // tslint:disable-next-line: deprecation
-
-}
-
-graficoInicial() {
-  
-  this.escondido = true;
-  console.log(this.escondido);
-}
-
-getListadetalhada(id: number) {
-  // pega id da coluna clicada
-  // retorna a lista de objecos com nome de cada interaçao
-  // e valor total dentro da iteraçao.
-}
-criaLabelAndValor() {
-
-this.listaDetalhada.forEach(obj => {
-  this.colunas.push(obj.name);
-  this.valores.push(obj.valor);
- });
+  const idClicado: number = this.myChart.chart.getElementAtEvent(event)[0]._label;
+  this.router.navigate(['/estatistica'], { state: { labelClicado: idClicado } });  console.log(idClicado);
 }
 
 }
+
