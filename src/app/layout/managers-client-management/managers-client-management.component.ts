7b9aac10e088184cc50323e9307eee256a4cf479
Filipe Bastias <filipebastias94@gmail.com>
2020-04-07 12:46:51 +0100

    Table added and showing all clients stored in API

--- src/app/layout/managers-client-management/managers-client-management.component.ts
+++ src/app/layout/managers-client-management/managers-client-management.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ClientsService } from 'src/app/core/services/clients.service';
 import { Client } from 'src/app/core/models/client';
+import { ColumnMode } from '@swimlane/ngx-datatable';
 
 @Component({
   selector: 'app-managers-client-management',
@@ -13,17 +14,43 @@ export class ManagersClientManagementComponent implements OnInit {
   
   constructor(
     private clients: ClientsService
-  ) { }
+  ) { 
+      this.fetch(data => {
+        this.rows = data;
+        setTimeout(() => {
+          this.loadingIndicator = false;
+        }, 1500);
+      }); 
+}
 
   ngOnInit(): void {
     this.getAllClients();
   }
+
   getAllClients() {
     this.clients.getAllClients(this.clientsData).subscribe((res:any) =>   {
       console.log('resultado', res);
-  });
-}
+    });
+  }
   
-  
+  /* TABLE */
+  rows = [];
+  loadingIndicator = true;
+  reorderable = true;
+
+  columns = [{ prop: 'name' }, { name: 'NIPC' }, { name: 'Potential Revenue', sortable: false }];
+
+  ColumnMode = ColumnMode;
+
+  fetch(cb) {
+    const req = new XMLHttpRequest();
+    req.open('GET', `http://localhost:8080/kpiManager/api/clients`);
+
+    req.onload = () => {
+      cb(JSON.parse(req.response));
+    };
+
+    req.send();
+  }
 
 }
