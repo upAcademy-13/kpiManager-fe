a36d87bd1c4bba73ad076d764371f97af81d232b
danielrossi6 <danieljosetavares@gmail.com>
2020-04-09 13:38:03 +0100

    Fixed column names and button active

--- src/app/layout/managers-client-management/managers-client-management.component.ts
+++ src/app/layout/managers-client-management/managers-client-management.component.ts
@@ -5,11 +5,11 @@ import { ColumnMode } from '@swimlane/ngx-datatable';
 import { HttpClient } from '@angular/common/http';
 import { ManagersService } from 'src/app/core/services/managers.service';
 import { User } from 'src/app/core/models/user';
-import { BsModalService, BsModalRef, ModalDirective } from 'ngx-bootstrap/modal';
+import { BsModalService, BsModalRef, ModalDirective, ModalOptions } from 'ngx-bootstrap/modal';
 import { Observable } from 'rxjs';
 import { InteractionsService } from 'src/app/core/services/interactions.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+declare var $: any;
 @Component({
   selector: 'app-managers-client-management',
   templateUrl: './managers-client-management.component.html',
@@ -21,6 +21,14 @@ export class ManagersClientManagementComponent implements OnInit {
   public clientsData: Client[] = [];
   public managersData: User[] = [];
   private apiUrl: 'http://localhost:8080/kpiManager/api';
+  modalRef: BsModalRef;
+  public units$: Observable<Object>;
+  selectUnit: string;
+  managerForm : FormGroup;
+  isEdit : boolean = false;
+isCreate : boolean = false;
+clientForm : FormGroup;
+isManager : boolean = false;
   
   constructor(
     private clients: ClientsService,
@@ -32,6 +40,7 @@ export class ManagersClientManagementComponent implements OnInit {
   ) { 
       this.fetch(data => {
         this.rows = data;
+        this.columns = [{ name: 'Name' }, { name: 'NIPC' }, {name: 'Actions'}]
         console.log(this.rows);
         
       }); 
@@ -40,29 +49,48 @@ export class ManagersClientManagementComponent implements OnInit {
   ngOnInit(): void {
 
     this.getFormData();
-    this.profileForm = new FormGroup({
+    this.managerForm = new FormGroup({
       name : new FormControl('',Validators.required),
       nipc : new FormControl('',Validators.required),
-     unit : new FormGroup(
-       {id: new FormControl('',Validators.required),
-     })
+      unit : new FormGroup(
+        {id: new FormControl('',Validators.required),
+      })
+    })
+    this.clientForm = new FormGroup({
+      name : new FormControl('',Validators.required),
+      nipc : new FormControl('',Validators.required),
     })
   }
 
+  
+
+
   getAllClients() {
+    
     this.clients.getAllClients(this.clientsData).subscribe((res:any) =>   {
       console.log('resultado', res);
       this.rows = [...res];
-      this.columns = [{ prop: 'name' }, { name: 'NIPC' }, { name: 'Potential Revenue', sortable: false }, { name: 'Unit ID' }];
+      this.columns = [{ name: 'Name' }, { name: 'NIPC' }, {name: 'Actions'}];
+      console.log(this.rows , " Clientes");
+      this.isManager = false;
     });
   }
 
   getAllManagers() {
+    
     this.managers.getAllManagers(this.managersData).subscribe((res:any) =>   {
+      
       console.log('resultadoM', res);
       this.rows = [...res];
-      this.columns = [{ prop: 'name' }, { name: 'Unit', sortable: false }];
+      this.columns = [{ name: 'Name' }, { name: 'Unit', sortable: false }, {name:'Actions'}];
+      console.log(this.rows , " Managers");
+      this.isManager = true;
     });
+  }
+
+  test(obj){
+    console.log(obj);
+
   }
   
   /* TABLE */
@@ -84,20 +112,12 @@ export class ManagersClientManagementComponent implements OnInit {
 
     req.send();
   }
-  modalRef: BsModalRef;
-  public units$: Observable<Object>;
-  selectUnit: string;
+
   private apiUrlClient = 'http://localhost:8080/kpiManager/api/clients';
   public client: Client = new Client();
-  name;
-  nipc;
-  unit;
-profileForm : FormGroup;
-  
-  
-  
 
 
+  
 
 
   openModal(template: TemplateRef<any>) {
@@ -112,17 +132,47 @@ public getFormData() {
   
 }
 
+public hide(){
+  this.profileForm.reset();
+  this.lgModal.hide();
+}
+
+
 @ViewChild('lgModal') public lgModal: ModalDirective;
 public addClient(){
   
-  this.http.post<any>(this.apiUrlClient,this.profileForm.value).subscribe(data => {
+  this.http.post<any>(this.apiUrlClient,this.clientForm.value).subscribe(data => {
   },
   error => {
     console.log(error);
   });
   this.http.get(this.apiUrlClient ).subscribe(data =>{
   })
+  this.clientForm.reset();
   this.lgModal.hide();
+
+}
+public add(){
+  this.isEdit = true;
+  this.isCreate = true;
+  this.isManager = false;
+  this.lgModal.show();
+  let element: HTMLElement = document.getElementById('client') as HTMLElement;
+  element.click();
+}
+
+
+public edit(row: any){
+  this.isEdit = true;
+  this.isCreate = true;
+  this.lgModal.show();
+
+  console.log(row);
+  this.profileForm.patchValue({
+name:    row.name,
+nipc: row.nipc,
+unit: row.unit
+  });
 }
 
 
