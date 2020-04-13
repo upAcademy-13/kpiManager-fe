27bfb996699b237fc3956d0db7817ddfbb8cf1c1
capitaofausto <>
2020-04-13 11:27:09 +0100

    elProfessor: paginator done

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -4,7 +4,7 @@ import { DatatableComponent, DataTableFooterComponent, DataTablePagerComponent }
 import { TableColumn, ColumnMode } from '@swimlane/ngx-datatable';
 import { FormBuilder } from '@angular/forms';
 import { DataInteraction } from 'src/app/core/models/dataInteration';
-import { ViewEncapsulation } from '@angular/core'
+import { ViewEncapsulation } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { DataService } from 'src/app/core/services/data.service';
 import { Page } from 'src/app/core/models/Page';
@@ -37,7 +37,7 @@ export class TableComponent implements OnInit {
   ColumnMode = ColumnMode;
   temp = [];
   rows = [];
-  inputSearch: string = "";
+  inputSearch = '';
   selectInteraction: string;
   selectBM: string;
   selectClient: string;
@@ -50,17 +50,19 @@ export class TableComponent implements OnInit {
     offset: 0,
     limit: 10,
     count: 0,
-  }
+  };
 
 
 
   ngOnInit() {
-    console.log('ngOnInit - start')
-    this.setPage({ offset: 0, limit: 10, count:0 });
-
-
-
-    console.log('ngOnInit - fim')
+    console.log('ngOnInit - start');
+    this.selectInteraction = !!window.history.state.selectInteraction ? window.history.state.selectInteraction : '';
+    this.selectBM = !!window.history.state.selectBM ? window.history.state.selectBM : '';
+    this.selectClient = !!window.history.state.selectClient ? window.history.state.selectClient : '';
+    this.selectUnit = !!window.history.state.selectUnit ? window.history.state.selectUnit : '';
+    this.selectWeek = !!window.history.state.selectWeek ? window.history.state.selectWeek : '';
+    this.setPage({ offset: 0, limit: 10, count: 0 });
+    console.log('ngOnInit - fim');
   }
 
     constructor(
@@ -74,8 +76,7 @@ export class TableComponent implements OnInit {
 
   refreshTable() {
     this.filterClearSelect();
-    this.inputSearch = "";
-    this.rows = this.temp;
+    this.filter(true);
   }
 
   fetch(cb) {
@@ -85,94 +86,90 @@ export class TableComponent implements OnInit {
       cb(JSON.parse(req.response));
     };
     req.send();
-    console.log("passou req", req)
+    console.log('passou req', req);
   }
 
   updateFilter(event) {
     this.filterClearSelect();
     const val = event.target.value.toLowerCase();
-    const temp = this.temp.filter(function (d) {
+    const temp = this.temp.filter(function(d) {
       return d.person.name.toLowerCase().indexOf(val) !== -1 || d.client.name.toLowerCase().indexOf(val) !== -1 ||
         d.unit.nameUnit.toLowerCase().indexOf(val) !== -1 || d.dateInteraction.toLowerCase().indexOf(val) !== -1 ||
         d.interactionType.interactionType.toLowerCase().indexOf(val) !== -1 || !val;
     });
     this.rows = temp;
     this.table.offset = 0;
-    console.log("passou", this.rows)
+    console.log('passou', this.rows);
   }
 
-  filter() {
-console.log(this.pageInfo);
-
-    this.rows = [];
-    let myselectWeek, myselectUnit, myselectClient, myselectBM, myselectInteration;
-    myselectBM = this.selectBM !== "" ? this.selectBM : null;
-    myselectClient = this.selectClient !== "" ? this.selectClient : null;
-    myselectUnit = this.selectUnit !== "" ? this.selectUnit : null;
-    myselectWeek = this.selectWeek!== "" ? this.selectWeek :null;
-    myselectInteration = this.selectInteraction !== "" ? this.selectInteraction : null;
-
+  filter(isFilter) {
+    console.log(isFilter);
+    if (isFilter) {// se for filtro tem de mudar para a primeira pagina se for paginaçao nao
+      this.table.offset = 0;
+      this.pageInfo.offset = 0;
+    }
+    console.log(this.pageInfo);
+    //this.rows = [];
+    let myselectWeek, myselectUnit, myselectClient, myselectBM, myselectInteration, startIndex;
+    myselectBM = this.selectBM !== '' ? this.selectBM : null;
+    myselectClient = this.selectClient !== '' ? this.selectClient : null;
+    myselectUnit = this.selectUnit !== '' ? this.selectUnit : null;
+    myselectWeek = this.selectWeek !== '' ? this.selectWeek : null;
+    myselectInteration = this.selectInteraction !== '' ? this.selectInteraction : null;
     let params = new HttpParams();
-
+    startIndex = this.pageInfo.offset * this.pageInfo.limit;
     params = params.append('week', myselectWeek);
     params = params.append('unit', myselectUnit);
     params = params.append('client', myselectClient);
     params = params.append('businessManagers', myselectBM);
     params = params.append('interaction', myselectInteration);
-    params = params.append('startIndex', this.pageInfo.offset.toString());
+    params = params.append('startIndex', startIndex.toString());
     params = params.append('quantity', this.pageInfo.limit.toString());
 
     this.filtro$ = this.http.get<Paginate>(this.apiUrl + 'interactions/filter', {
-      params: params
+      params
     });
-    this.filtro$.subscribe((res:Paginate) => {
+    this.filtro$.subscribe((res: Paginate) => {
       this.rows = [...res.elements];
       console.log(this.rows);
       this.totalElements = res.totalElements;
-      this.table.offset = 0;
+      //this.table.offset = 0;
       this.cdr.detectChanges();
-    })
+    });
   }
 
   filterClearSelect() {
-
-    this.selectBM = "";
-    this.selectClient="";
-    this.selectWeek="";
-    this.selectUnit="";
-    this.selectInteraction="";
-    //fazer o clear da paginacao para voltar à posicao 0 (pagina 1)
+    this.inputSearch = '';
+    this.selectBM = '';
+    this.selectClient = '';
+    this.selectWeek = '';
+    this.selectUnit = '';
+    this.selectInteraction = '';
+    // fazer o clear da paginacao para voltar à posicao 0 (pagina 1)
   }
 
-  setPage(pageInfo: { count: number, limit: number, offset: number }){
-    
+  setPage(pageInfo: { count: number, limit: number, offset: number }) {
+
     this.pageInfo = pageInfo;
-    console.log("pageInfo", pageInfo);
-    
-    this.selectInteraction = !!window.history.state.selectInteraction ? window.history.state.selectInteraction : '';
-    this.selectBM = !!window.history.state.selectBM ? window.history.state.selectBM : '';
-    this.selectClient = !!window.history.state.selectClient ? window.history.state.selectClient : '';
-    this.selectUnit = !!window.history.state.selectUnit ? window.history.state.selectUnit : '';
-    this.selectWeek = !!window.history.state.selectWeek ? window.history.state.selectWeek : '';
-    if (this.selectInteraction !== '' || this.selectUnit !== '' || this.selectUnit !== '' || this.selectWeek !== '' || this.selectClient) {
+    console.log('pageInfo', pageInfo);
+
+    this.filter(false);
+    /* if (this.selectInteraction !== '' || this.selectUnit !== '' || this.selectUnit !== '' || this.selectWeek !== '' || this.selectClient) {
       this.filter();
     } else {
       this.fetch(data => {
         this.totalElements = data.length;
-        // this.temp = [...data];
-        // this.rows = this.temp;
-
-        this.rows = [];
+        //this.rows = [];
         let myselectWeek, myselectUnit, myselectClient, myselectBM, myselectInteration;
-        myselectBM = this.selectBM !== "" ? this.selectBM : null;
-        myselectClient = this.selectClient !== "" ? this.selectClient : null;
-        myselectUnit = this.selectUnit !== "" ? this.selectUnit : null;
-        myselectWeek = this.selectWeek!== "" ? this.selectWeek :null;
-        myselectInteration = this.selectInteraction !== "" ? this.selectInteraction : null;
+        myselectBM = this.selectBM !== '' ? this.selectBM : null;
+        myselectClient = this.selectClient !== '' ? this.selectClient : null;
+        myselectUnit = this.selectUnit !== '' ? this.selectUnit : null;
+        myselectWeek = this.selectWeek !== '' ? this.selectWeek : null;
+        myselectInteration = this.selectInteraction !== '' ? this.selectInteraction : null;
 
         let params = new HttpParams();
-        console.log("offsetpageNumber = ",this.pageInfo.offset, "pageSize = ", pageInfo.limit, "count = ", pageInfo.count);
-        
+        console.log('offsetpageNumber = ', this.pageInfo.offset, 'pageSize = ', pageInfo.limit, 'count = ', pageInfo.count);
+
         params = params.append('week', myselectWeek);
         params = params.append('unit', myselectUnit);
         params = params.append('client', myselectClient);
@@ -180,22 +177,22 @@ console.log(this.pageInfo);
         params = params.append('interaction', myselectInteration);
         params = params.append('startIndex', this.pageInfo.offset.toString());
         params = params.append('quantity', this.pageInfo.limit.toString());
-    
-        this.dataPagination$ = this.http.get<Paginate>(this.apiUrl + 'interactions/filter', { params: params });
-        
-        this.dataPagination$.subscribe((res:Paginate) => {
-        console.log("dataPagination = ", res);
-        this.temp = [...res.elements];
-        this.rows = this.temp;
-        console.log("rows aqui =",this.rows);
-        this.table.offset = 0;
-        this.page.size = pageInfo.limit;
-        this.totalElements = res.totalElements;
-        this.page.pageNumber = pageInfo.offset;
-        this.cdr.detectChanges();
-      })
-      })
-    }
+
+        this.dataPagination$ = this.http.get<Paginate>(this.apiUrl + 'interactions/filter', { params });
+
+        this.dataPagination$.subscribe((res: Paginate) => {
+          console.log('dataPagination = ', res);
+          this.temp = [...res.elements];
+          this.rows = this.temp;
+          console.log('rows aqui =', this.rows);
+          this.table.offset = 0;
+          this.page.size = pageInfo.limit;
+          this.totalElements = res.totalElements;
+          this.page.pageNumber = pageInfo.offset;
+          this.cdr.detectChanges();
+        });
+      });
+    } */
 
 
   }
@@ -207,6 +204,6 @@ export interface Paginate {
 
   elements: Object[];
   totalElements: number;
-  
-} 
+
+}
 
