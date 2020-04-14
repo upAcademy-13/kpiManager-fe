027e2777148d6a2c396705315b9039b79d3e5a19
Filipe Marques <filipeavmarques@gmail.com>
2020-04-14 19:26:24 +0100

    last fix

--- src/app/layout/dashboard/conversao-semanal/conversao-semanal.component.ts
+++ src/app/layout/dashboard/conversao-semanal/conversao-semanal.component.ts
@@ -50,7 +50,21 @@ export class ConversaoSemanalComponent implements OnInit {
     this.breakpoint = event.target.innerWidth <= 1000 ? 1 : 2;
   }
 
+  generateColor(length) {
+    let data = [];
+    for (let index = 0; index < length; index++) {
+      var colors = {
+        r: Math.floor(253 + Math.random() * 2),
+        g: Math.floor(50 + Math.random() * 161),
+        b: Math.floor(40 + Math.random() * 44)
+      };
+      data.push(`rgba(${colors.r}, ${colors.g}, ${colors.b}, 0.8)`);
+    }
+    return data;
+  }
+
   chartit() {
+    const colors = this.generateColor(2);
     this.myChart = new Chart("chartContracts", {
       type: "line",
       data: {
@@ -60,108 +74,32 @@ export class ConversaoSemanalComponent implements OnInit {
             label: "Signed Contracts",
             fill: false,
             data: this.acceptedContractsCount,
-            // backgroundColor: [
-            //   /*    "rgba(255, 99, 132, 0.5)",
-            //   "rgba(54, 162, 235, 0.5)",
-            //   "rgba(255, 206, 86, 0.5)",
-            //   "rgba(246, 8, 12, 0.5)",
-            //   "rgba(153, 102, 255, 0.5)",
-            //   "rgba(255, 159, 64, 0.5)",
-            //   "rgba(33, 246, 33, 0.5)",
-            //   "rgba(194, 30, 30, 0.5)",
-            //   "rgba(112, 56, 4, 0.5)",
-            //   "rgba(28, 28, 246, 0.5)",
-            //   "rgba(235, 122, 9, 0.5)",
-            //   "rgba(8, 246, 127, 0.5)", */
-            //   "rgba(242, 102, 9, 0.8)",
-            //   "rgba(242, 122, 24,  0.8)",
-            //   "rgba(237, 154, 37,  0.8)",
-            //   "rgba(255, 175, 48,  0.8)",
-            //   "rgba(255, 192, 93,  0.8)",
-            // ],
+            backgroundColor: [
+              colors[0]
+            ],
             borderColor: [
-              /*    "rgba(255, 99, 132, 1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-              "rgba(246, 8, 12, 1)",
-              "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 64, 1)",
-              "rgba(33, 246, 33, 1)",
-              "rgba(194, 30, 30, 1)",
-              "rgba(112, 56, 4, 1)",
-              "rgba(28, 28, 246, 1)",
-              "rgba(235, 122, 9, 1)",
-              "rgba(8, 246, 127, 1)", */
-              "rgba(242, 102, 9, 1)",
-              // "rgba(242, 122, 24, 1)",
-              // "rgba(237, 154, 37, 1)",
-              // "rgba(255, 175, 48, 1)",
-              // "rgba(255, 192, 93, 1)",
+              colors[0]
             ],
             borderWidth: 2,
             hoverBorderWidth: 3,
             hoverBorderColor: [
-              /* "rgba(255, 99, 132, 1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-              "rgba(246, 8, 12, 1)",
-              "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 64, 1)",
-              "rgba(33, 246, 33, 1)",
-              "rgba(194, 30, 30, 1)",
-              "rgba(112, 56, 4, 1)",
-              "rgba(28, 28, 246, 1)",
-              "rgba(235, 122, 9, 1)",
-              "rgba(8, 246, 127, 1)", */
-              "rgba(242, 102, 9, 1)",
-              // "rgba(242, 122, 24, 1)",
-              // "rgba(237, 154, 37, 1)",
-              // "#rgba(255, 175, 48, 1)",
-              // "#rgba(255, 192, 93, 1)",
+              colors[0]
             ],
           },
           {
             data: this.contractsCount,
             label: "All Contracts",
             fill: false,
-            // backgroundColor: [
-            //   /*    "rgba(255, 99, 132, 0.5)",
-            //   "rgba(54, 162, 235, 0.5)",
-            //   "rgba(255, 206, 86, 0.5)",
-            //   "rgba(246, 8, 12, 0.5)",
-            //   "rgba(153, 102, 255, 0.5)",
-            //   "rgba(255, 159, 64, 0.5)",
-            //   "rgba(33, 246, 33, 0.5)",
-            //   "rgba(194, 30, 30, 0.5)",
-            //   "rgba(112, 56, 4, 0.5)",
-            //   "rgba(28, 28, 246, 0.5)",
-            //   "rgba(235, 122, 9, 0.5)",
-            //   "rgba(8, 246, 127, 0.5)", */
-            //   "rgba(242, 102, 9, 0.8)",
-            //   "rgba(242, 122, 24,  0.8)",
-            //   "rgba(237, 154, 37,  0.8)",
-            //   "rgba(255, 175, 48,  0.8)",
-            //   "rgba(255, 192, 93,  0.8)",
-            // ],
+            backgroundColor: [
+              colors[1]
+            ],
             borderColor: [
-              "rgb(128,128,128)",
-              // "rgba(255, 99, 132, 1)",
-              // "rgba(242, 102, 9, 1)",
-              // "rgba(242, 122, 24, 1)",
-              // "rgba(237, 154, 37, 1)",
-              // "rgba(255, 175, 48, 1)",
-              // "rgba(255, 192, 93, 1)",
+              colors[1]
             ],
             borderWidth: 2,
             hoverBorderWidth: 3,
             hoverBorderColor: [
-              "rgb(128,128,128)",
-              // "rgba(255, 99, 132, 1)",
-              // "rgba(242, 102, 9, 1)",
-              // "rgba(242, 122, 24, 1)",
-              // "rgba(237, 154, 37, 1)",
-              // "#rgba(255, 175, 48, 1)",
-              // "#rgba(255, 192, 93, 1)",
+              colors[1]
             ],
           },
         ],
